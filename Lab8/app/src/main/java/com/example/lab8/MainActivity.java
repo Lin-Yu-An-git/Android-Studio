961f9eeef5ab8e999caf8d332d748df23c9c582e
Lin-Yu-An-git <t109360112@ntut.org.tw>
2022-11-25 09:56:37 +0800

    11/25

---
+++ Lab8/app/src/main/java/com/example/lab8/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.service_start).setOnClickListener(v -> {
+            Intent intent = new Intent(this,MyService.class);
+            startService(intent);
+            Toast.makeText(this, "啟動Service", Toast.LENGTH_SHORT).show();
+            finish();
+        });
+    }
+}
